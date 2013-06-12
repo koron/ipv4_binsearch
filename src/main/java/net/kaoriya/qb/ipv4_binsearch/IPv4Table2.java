fe00330df530ed8f65f1f0495f367bb247af992c
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-06-12 15:18:13 +0900

    use SerializedArray

--- src/main/java/net/kaoriya/qb/ipv4_binsearch/IPv4Table2.java
+++ src/main/java/net/kaoriya/qb/ipv4_binsearch/IPv4Table2.java
@@ -1,5 +1,11 @@
 package net.kaoriya.qb.ipv4_binsearch;
 
+import java.io.IOException;
+
+import net.kaoriya.qb.serialized_array.BytesStore;
+import net.kaoriya.qb.serialized_array.Converter;
+import net.kaoriya.qb.serialized_array.SerializedArray;
+
 /**
  * Less objects version of IPv4Table.
  */
@@ -7,9 +13,15 @@ public final class IPv4Table2<T> extends IPv4TableBase<T>
 {
     private final IntRangeIndexArray indexArray;
 
-    public IPv4Table2(IPv4Table<T> src) {
+    private final SerializedArray<T> dataArray;
+
+    public IPv4Table2(
+            IPv4Table<T> src,
+            Converter<T> converter,
+            BytesStore bytesStore)
+    {
         this.indexArray = new IntRangeIndexArray(src.rangeTable);
-        // TODO:
+        this.dataArray = new SerializedArray<T>(converter, bytesStore);
     }
 
     public void add(int start, int end, T data) {
@@ -26,7 +38,10 @@ public final class IPv4Table2<T> extends IPv4TableBase<T>
     }
 
     private T get(int index) {
-        // TODO:
-        return null;
+        try {
+            return this.dataArray.get(index);
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
