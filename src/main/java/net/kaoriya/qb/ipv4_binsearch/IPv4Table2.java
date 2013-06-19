efc37ada139529b9a6aed345dcdaf2480f55188f
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-06-19 15:14:56 +0900

    use OffHeapAray

--- src/main/java/net/kaoriya/qb/ipv4_binsearch/IPv4Table2.java
+++ src/main/java/net/kaoriya/qb/ipv4_binsearch/IPv4Table2.java
@@ -1,27 +1,35 @@
 package net.kaoriya.qb.ipv4_binsearch;
 
+import java.io.Closeable;
 import java.io.IOException;
 
-import net.kaoriya.qb.serialized_array.BytesStore;
 import net.kaoriya.qb.serialized_array.Converter;
-import net.kaoriya.qb.serialized_array.SerializedArray;
+import net.kaoriya.qb.serialized_array.HeapArray;
+import net.kaoriya.qb.serialized_array.OffHeapArray;
 
 /**
  * Less objects version of IPv4Table.
  */
 public final class IPv4Table2<T> extends IPv4TableBase<T>
+    implements Closeable
 {
     private final IntRangeIndexArray indexArray;
 
-    private final SerializedArray<T> dataArray;
+    private final OffHeapArray<T> dataArray;
 
     public IPv4Table2(
             IPv4Table<T> src,
-            Converter<T> converter,
-            BytesStore bytesStore)
+            Converter<T> converter)
+        throws Exception
     {
         this.indexArray = new IntRangeIndexArray(src.rangeTable);
-        this.dataArray = new SerializedArray<T>(converter, bytesStore);
+
+        HeapArray heapArray = new HeapArray<T>(converter);
+        for (int i = 0, max = src.rangeTable.arrayList.size(); i < max; ++i) {
+            heapArray.add(src.rangeTable.getData(i));
+        }
+
+        this.dataArray = new OffHeapArray(heapArray);
     }
 
     public void add(int start, int end, T data) {
@@ -43,5 +51,10 @@ public final class IPv4Table2<T> extends IPv4TableBase<T>
         } catch (IOException e) {
             return null;
         }
+    }
+
+    public void close()
+    {
+        this.dataArray.close();
     }
 }
